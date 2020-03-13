6f39cc1d99a2144a7f5b953df28160c76d5e2d35
Siddhant Gupta <siddhantgupta89@gmail.com>
2020-03-13 12:12:38 +0530

    qerty

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static('public'));
 
 var server = app.listen(8081, function() {
     var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
+    console.log(`Server started at ${server.address()}, ${port}`);
 });
